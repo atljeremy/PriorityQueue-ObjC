091c4e75c9ecef6024b908ebe4f1a6945679dd07
Jeremy Fox <atljeremy@me.com>
2014-12-30 11:26:31 -0500

    Initial Commit

---
+++ PriorityQueue/PriorityQueue.m
@@ -0,0 +1,155 @@
+//
+//  PriorityQueue.m
+//  PriorityQueue
+//
+//  Created by Jeremy Fox on 12/29/14.
+//  Copyright (c) 2014 Jeremy Fox. All rights reserved.
+//
+
+#import "PriorityQueue.h"
+
+@interface PriorityQueue ()
+@property (nonatomic, strong) NSMutableArray* queue;
+@end
+
+@implementation PriorityQueue
+
+- (id)init {
+    if (self = [super init]) {
+        _queue = [@[] mutableCopy];
+    }
+    return self;
+}
+
+- (id)initWithObjects:(NSSet*)objects
+{
+    if (self = [super init]) {
+        _queue = [@[] mutableCopy];
+        for (id<NSObject> object in objects) {
+            [self add:object];
+        }
+    }
+    return self;
+}
+
+- (id)initWithCapacity:(int)capacity
+{
+    if (self = [super init]) {
+        _queue = [NSMutableArray arrayWithCapacity:capacity];
+    }
+    return self;
+}
+
+- (BOOL)isEmpty
+{
+    return (self.size > 0);
+}
+
+- (NSUInteger)size
+{
+    return self.queue.count;
+}
+
+- (BOOL)contains:(id<NSObject>)object
+{
+    return [self.queue containsObject:object];
+}
+
+- (void)clear
+{
+    [self.queue removeAllObjects];
+}
+
+- (BOOL)add:(id<NSObject>)object
+{
+    [self insertObjectUsingBinarySearch:object];
+    return [self contains:object];
+}
+
+- (BOOL)remove:(id<NSObject>)object
+{
+    [self.queue removeObject:object];
+    return ![self contains:object];
+}
+
+- (id<NSObject>)peek
+{
+    return self.queue[0];
+}
+
+- (id<NSObject>)poll
+{
+    id<NSObject> object = self.queue[0];
+    [self.queue removeObject:object];
+    return object;
+}
+
+- (NSArray*)toArray
+{
+    return self.queue.copy;
+}
+
+#pragma mark ----------------------
+#pragma mark Private
+#pragma mark ----------------------
+
+- (NSComparator)comparator
+{
+    if (_comparator) {
+        return _comparator;
+    } else {
+        return ^NSComparisonResult(NSNumber* obj1, NSNumber* obj2) {
+            NSComparisonResult result = NSOrderedSame;
+            if ([obj1 intValue] < [obj2 intValue]) {
+                result = NSOrderedAscending;
+            } else if ([obj1 intValue] > [obj2 intValue]) {
+                result = NSOrderedDescending;
+            }
+            return result;
+        };
+    }
+}
+
+- (void)insertObjectUsingBinarySearch:(id<NSObject>)object
+{
+    if (self.size == 0) {
+        [self.queue addObject:object];
+        return;
+    }
+    
+    NSUInteger mid = 0;
+    NSUInteger min = 0;
+    NSUInteger max = self.queue.count - 1;
+    BOOL found = NO;
+    
+    while (min <= max) {
+        
+        mid = (min + max) / 2;
+        
+        NSComparisonResult result = self.comparator(object, self.queue[mid]);
+        
+        if (result == NSOrderedSame) {
+            mid++;
+            found = YES;
+            break;
+        } else if (result == NSOrderedAscending) {
+            max = mid - 1;
+            if (max == NSUIntegerMax) {
+                found = YES;
+                break;
+            }
+        } else if (result == NSOrderedDescending) {
+            min = mid + 1;
+        }
+    }
+    
+    if (found) {
+        // Index found at mid
+        [self.queue insertObject:object atIndex:mid];
+    } else {
+        // Index not found, use min
+        [self.queue insertObject:object atIndex:min];
+    }
+}
+
+@end
---
+++ PriorityQueue/main.m
@@ -0,0 +1,40 @@
+//
+//  main.m
+//  PriorityQueue
+//
+//  Created by Jeremy Fox on 12/29/14.
+//  Copyright (c) 2014 Jeremy Fox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PriorityQueue.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSArray* objects = @[@89, @1, @2, @33, @56, @4, @5, @67, @10, @7, @9, @8, @11, @12, @13, @14, @15, @16, @17, @18, @134, @77, @31, @19, @20];
+        
+        PriorityQueue* queue = [PriorityQueue new];
+        
+        for (NSNumber* num in objects) {
+            [queue add:num];
+        }
+        
+        id<NSObject> object = [queue peek];
+        
+        BOOL removed = [queue remove:@1];
+        removed = [queue remove:@3];
+        removed = [queue remove:@8];
+
+        NSLog(@"Size: %lu", (unsigned long)queue.size);
+        
+        object = [queue poll];
+        
+        [queue add:@1];
+        
+        object = [queue peek];
+        
+        [queue add:@25];
+    }
+    return 0;
+}
